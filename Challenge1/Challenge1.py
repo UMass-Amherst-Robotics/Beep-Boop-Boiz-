d32decb39b8e1a5e687a57a2d12014998f7292c5
Andy <agutierrez@umass.edu>
2020-04-18 17:05:57 -0400

    first Test
    Testing code to try to get the robot to move forward

---
+++ Challenge1/Challenge1.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as gpio
+import time
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+def forward(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+forward(1)

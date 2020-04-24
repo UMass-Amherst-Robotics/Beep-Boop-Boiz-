cafc0aca2e5b32a167d26740e1d2d2da509e2f7c
Andy <agutierrez@umass.edu>
2020-04-24 12:46:49 -0400

    Implementation of sonar and pivot
    basic implementation of the sonar sensor and pivot command

--- Challenge1/Challenge1.py
+++ Challenge1/Challenge1.py
@@ -1,8 +1,15 @@
 import RPi.GPIO as gpio
 import time
+TRIG = 23
+ECHO = 24
 
 def init():
+    gpio.setmode(gpio.BCM)
     gpio.setmode(gpio.BOARD)
+    gpio.setup(TRIG, gpio.OUT)
+    gpio.setup(ECHO, gpio.IN)
+    gpio.output(TRIG, False)
+    time.sleep(2)
     gpio.setup(7, gpio.OUT)
     gpio.setup(11, gpio.OUT)
     gpio.setup(13, gpio.OUT)
@@ -53,9 +60,50 @@ def veer_left_forward(tf):
     time.sleep(tf)
     gpio.cleanup()
 
+def pivot_right(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, False)
+    gpio.output(15, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def pivot_right(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(11, False)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def get_distance():
+    gpio.output(TRIG, True)
+    time.sleep(0.00001)
+    gpio.output(TRIG, False)
+    # initializing variables
+    pulse_start = 0
+    pulse_end = 0
+    while gpio.input(Echo) == 0:
+        pulse_start = time.time()
+    while gpio.input(Echo) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    distance_cm = pulse_duration * 17150
+    distance_cm = round(distance_cm, 2)
+    return distance_cm
+
+def main():
+    pivot_right(1)
+    time.sleep(5)
+    pivot_left(1)
+    print(get_distance())
 
 
-backwards(1)
+
+
 
 
 
